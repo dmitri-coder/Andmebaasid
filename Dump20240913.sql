6a2e1ca2f506bfe7465b1bef96c4391700ac2243
dmitri-coder <57408999+dmitri-coder@users.noreply.github.com>
2024-09-13 10:13:09 +0300

    Lisasin failid

---
+++ Dump20240913.sql
@@ -0,0 +1,167 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dzverkov
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `studentid` bigint DEFAULT NULL,
+  `comment` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `studentid` (`studentid`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`studentid`) REFERENCES `student` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (1,1,'Õppis väga usinasti eksamiks.'),(2,2,'Ei osalenud loengutel.'),(3,3,'Vajab lisatuge kodutöödes.'),(4,4,'Oli aktiivne grupiaruteludes.'),(5,5,'Magas klassis.'),(6,6,'Kutsus klassivenda ahviks.');
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!50001 DROP VIEW IF EXISTS `comments`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `comments` AS SELECT 
+ 1 AS `id`,
+ 1 AS `firstname`,
+ 1 AS `lastname`,
+ 1 AS `comment`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course` (
+  `code` varchar(8) NOT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course`
+--
+
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+INSERT INTO `course` VALUES ('AB','Andmebaasid'),('MATA','Matemaatika');
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stgroup`
+--
+
+DROP TABLE IF EXISTS `stgroup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stgroup` (
+  `code` varchar(8) NOT NULL,
+  `description` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stgroup`
+--
+
+LOCK TABLES `stgroup` WRITE;
+/*!40000 ALTER TABLE `stgroup` DISABLE KEYS */;
+INSERT INTO `stgroup` VALUES ('MATA','Matemaatika'),('MM','Multimeedia Spetsialist'),('TA','Tarkvaraarendaja'),('VM','Visuaalmeedia');
+/*!40000 ALTER TABLE `stgroup` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `id` bigint NOT NULL,
+  `firstname` varchar(256) DEFAULT NULL,
+  `lastname` varchar(256) DEFAULT NULL,
+  `groupcode` varchar(8) DEFAULT NULL,
+  `birthdate` varchar(16) DEFAULT NULL,
+  `course` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `groupcode` (`groupcode`),
+  KEY `course` (`course`),
+  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`groupcode`) REFERENCES `stgroup` (`code`),
+  CONSTRAINT `student_ibfk_2` FOREIGN KEY (`course`) REFERENCES `course` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'Sermo','Kutt','MM','2006.06.06','AB'),(2,'Mari','Huaana','VM','1999.01.02','MATA'),(3,'Heli','Kopter','VM','2000.12.04','MATA'),(4,'Marja','Moos','MM','2004.03.01','MATA'),(5,'Ülle','Doos','VM','2005.01.01','AB'),(6,'Kristofer','Kumami','MM','2006.07.01','MATA'),(7,'Maks','Moorits','TA','2006.02.01','AB'),(8,'Kris','Kust','MM','2006.03.04','AB'),(9,'Hannes','Pakler','TA','2006.01.03','AB'),(10,'Ari','Matti','TA','2006.08.01','AB'),(11,'Risto','Alev','VM','2006.06.03','AB'),(12,'Toivo','Vesi','MM','2006.01.05','AB'),(13,'Matti','Kivi','MM','2008.01.03','AB'),(14,'Aino','Talv','VM','2007.01.09','AB'),(15,'Anna','Kevad','TA','2001.01.03','AB'),(16,'Anna','Musu','VM','2002.01.08','MATA'),(17,'Hans','Kruvi','TA','2003.01.03','MATA'),(18,'Joosep','Tuvi','TA','2004.01.07','AB'),(19,'Cristofer','Maja','MM','2005.01.03','AB'),(20,'Andrus','Terass','TA','2000.01.03','MATA');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `comments`
+--
+
+/*!50001 DROP VIEW IF EXISTS `comments`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `comments` AS select `comment`.`studentid` AS `id`,`student`.`firstname` AS `firstname`,`student`.`lastname` AS `lastname`,`comment`.`comment` AS `comment` from (`student` join `comment` on((`student`.`id` = `comment`.`studentid`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-13 10:03:53
